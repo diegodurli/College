107bfedd520f2bd6f38711498c396febe4c84e7b
Diego Durli <dgodurli@gmail.com>
2012-04-01 14:35:20 -0300

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'bcrypt-ruby'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'twitter-bootstrap-rails'
+end
+
+group :test do
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+College::Application.config.secret_token = 'f411663fb799e6a09ef2955a5aaeac61eac2275d84f79f660adfe8ce90df98d3551e90f274f4d9b0940949b400ebfb77fe3c1f3de13b14e7dc6724ea504fe806'
