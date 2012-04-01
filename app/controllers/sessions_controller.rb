1a8318efb474ab808c31bd63d536e92e90361a63
Diego Durli <dgodurli@gmail.com>
2012-04-01 20:02:36 -0300

    Added user signup and login/logout functionalities

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      cookies.permanent[:auth_token] = user.auth_token
+      redirect_to root_path, notice: 'User was logged in successfully.'
+    else
+      flash.now.notice = 'Your email or password is invalid.'
+      render :new
+    end
   end
 
   def destroy
+    cookies.delete(:auth_token)
+    redirect_to root_path, notice: 'User was logged out successfully.'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
+  attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
   has_many :subjects
   has_many :schedules
-  attr_accessible :name, :email, :password, :password_confirmation
 
   validates_presence_of :name, :email, :password, :password_confirmation
   validates_uniqueness_of :email
