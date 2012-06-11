ff286d2ab08c62f8800a403c92ecd3c659b07b3a
Diego Durli <dgodurli@gmail.com>
2012-06-11 01:32:09 -0300

    Added About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 College::Application.routes.draw do
-  
+
   root to: 'pages#index'
 
   get 'login' , to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
+  get 'about' , to: 'pages#about'
 
   resources :sessions
   resources :users do
